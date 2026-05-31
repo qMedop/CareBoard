7da44c9ddef33191d6837bcaad832a74c83f538b
Mohamed Saleh <95769954+qMedop@users.noreply.github.com>
2026-06-01 00:42:11 +0300

    update the sideNav on mobile

--- src/assets/icons/Icon.jsx
+++ src/assets/icons/Icon.jsx
@@ -2346,3 +2346,26 @@ export const SearchIcon = ({ className = "", size }) => {
     </div>
   );
 };
+export const ArrowRightLineIcon = ({ className = "", size, active }) => {
+  const style = size
+    ? {
+        width: size,
+        height: size,
+      }
+    : {};
+
+  return (
+    <div style={style} className={`svg ${className}`}>
+      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M8.91016 19.9201L15.4302 13.4001C16.2002 12.6301 16.2002 11.3701 15.4302 10.6001L8.91016 4.08008"
+          stroke="#f1f1f1"
+          strokeWidth="1.5"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>
+      </svg>
+    </div>
+  );
+};
--- src/components/sideNav/SideNav.jsx
+++ src/components/sideNav/SideNav.jsx
@@ -1,22 +1,36 @@
 import { NavLink, useLocation } from "react-router-dom";
 import styles from "./SideNav.module.css";
 import {
+  ArrowRightIcon,
+  ArrowRightLineIcon,
   CalendarIcon,
   GameIcon,
   HomeIcon,
+  MenuDotsHoriantalIcon,
   MoneyIcon,
   NoteIcon,
   SettingsIcon,
+  ThreeLinesDashedIcon,
   TimekIcon,
   ToDoIcon,
   WebsiteIcon,
 } from "../../assets/icons/Icon";
 import { useEffect, useState } from "react";
 import { useData } from "../../contexts/AuthContext";
+import { useTime } from "../../contexts/TimeContext";
+import CustomButton from "../button/Button";
+import { usePopup } from "../../contexts/PopupContext";
 function SideNav() {
   const location = useLocation();
   const { currentUser } = useData();
+  const { isMobile } = useTime();
   const [activePage, setActivePage] = useState(location.pathname);
+  const { openPopup, closePopup: closeContextPopup } = usePopup();
+
+  const openMobileMenu = (e) => {
+    openPopup("contextual", () => <MobileMenu />, e.currentTarget, "TopRight");
+  };
+
   useEffect(() => {
     setActivePage(location.pathname);
   }, [location.pathname]);
@@ -49,37 +63,121 @@ function SideNav() {
               <NoteIcon active={activePage.startsWith("/notes")} />
             </NavLink>
           </div>
-          <div>
-            <NavLink to="/games">
-              <GameIcon active={activePage.startsWith("/games")} />
-            </NavLink>
-          </div>
-          <div>
-            <NavLink to="/time">
-              <TimekIcon active={activePage.startsWith("/time")} />
-            </NavLink>
-          </div>
-          <div>
-            <NavLink to="/money">
-              <MoneyIcon active={activePage.startsWith("/money")} />
-            </NavLink>
-          </div>
+          {isMobile && (
+            <div>
+              <CustomButton
+                ClickEffect={false}
+                className="default"
+                onClick={(e) => {
+                  openMobileMenu(e);
+                }}
+              >
+                <ArrowRightLineIcon />
+              </CustomButton>
+            </div>
+          )}
+          {!isMobile && (
+            <>
+              <div>
+                <NavLink to="/games">
+                  <GameIcon active={activePage.startsWith("/games")} />
+                </NavLink>
+              </div>
+              <div>
+                <NavLink to="/time">
+                  <TimekIcon active={activePage.startsWith("/time")} />
+                </NavLink>
+              </div>
+              <div>
+                <NavLink to="/money">
+                  <MoneyIcon active={activePage.startsWith("/money")} />
+                </NavLink>
+              </div>
+            </>
+          )}
         </div>
       </div>
-      <div className={styles.bottom}>
-        <div className={styles.profile}>
-          <NavLink to="/profile">
-            <img src={currentUser.pfpUrl} alt="" />
-          </NavLink>
-        </div>
-        <div className={styles.settings}>
-          <NavLink to="/settings">
-            <SettingsIcon active={activePage.startsWith("/settings")} />
-          </NavLink>
-        </div>
-      </div>
+      {!isMobile && (
+        <>
+          <div className={styles.bottom}>
+            <div className={styles.profile}>
+              <NavLink to="/profile">
+                <img src={currentUser.pfpUrl} alt="" />
+              </NavLink>
+            </div>
+            <div className={styles.settings}>
+              <NavLink to="/settings">
+                <SettingsIcon active={activePage.startsWith("/settings")} />
+              </NavLink>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
 
+function MobileMenu() {
+  const [activePage, setActivePage] = useState(location.pathname);
+  const { currentUser } = useData();
+  return (
+    <div className={styles.mobileMoreMenu}>
+      <div>
+        <CustomButton
+          link={true}
+          href={"/games"}
+          className={"default"}
+          ClickEffect={"scale"}
+        >
+          <GameIcon active={activePage.startsWith("/games")} />
+          <p>Games</p>
+        </CustomButton>
+      </div>
+      <div>
+        <CustomButton
+          link={true}
+          href={"/time"}
+          className={"default"}
+          ClickEffect={"scale"}
+        >
+          <TimekIcon active={activePage.startsWith("/time")} />
+          <p>Time</p>
+        </CustomButton>
+      </div>
+      <div>
+        <CustomButton
+          link={true}
+          href={"/money"}
+          className={"default"}
+          ClickEffect={"scale"}
+        >
+          <MoneyIcon active={activePage.startsWith("/money")} />
+          <p>Money</p>
+        </CustomButton>
+      </div>
+      <div className={styles.settings}>
+        <CustomButton
+          link={true}
+          href={"/settings"}
+          className={"default"}
+          ClickEffect={"scale"}
+        >
+          <SettingsIcon active={activePage.startsWith("/settings")} />
+          <p>Settings</p>
+        </CustomButton>
+      </div>
+      <div className={styles.profile}>
+        <CustomButton
+          link={true}
+          href={"/profile"}
+          className={"default"}
+          ClickEffect={"scale"}
+        >
+          <img src={currentUser.pfpUrl} alt="" />
+          <p>Profile</p>
+        </CustomButton>
+      </div>
+    </div>
+  );
+}
 export default SideNav;
